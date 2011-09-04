fe9101bec3edb3b174a9db6da1ed447f5074baec
Marco Sero <sero.marco@gmail.com>
2011-09-04 23:24:01 +0200

    Version 0.2
    Changelog: - solved problem with push notifications

--- Tweak.mm
+++ Tweak.mm
@@ -1,39 +1,30 @@
-/**
- *
- * Author: Marco Sero
- * Version: 0.1
- * Copyright 2011 Marco Sero
- *
- *  This file is part of SnoozeOrStop.
- 
- SnoozeOrStop is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
- 
- toSync is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with toSync; if not, write to the Free Software
- Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
+#import <UIKit/UIKit2.h>
 #import <SpringBoard/SpringBoard.h>
+#import <CaptainHook/CaptainHook.h>
 
-%hook SBRemoteLocalNotificationAlert 
+%hook SBRemoteLocalNotificationAlert
+
+static BOOL IsMobileTimerAlarm(SBRemoteLocalNotificationAlert *self)
+{
+	return [[CHIvar(self, _app, SBApplication *) displayIdentifier] isEqualToString:@"com.apple.mobiletimer"];
+}
 
 - (void)configure:(BOOL)configure requirePasscodeForActions:(BOOL)actions
 {
-	UIAlertView *alertView = [self alertSheet];
-    
-	alertView.title = @"Alarm";
-	alertView.cancelButtonIndex = nil;
-    [alertView addButtonWithTitle:@"Snooze..."];
-	[alertView addButtonWithTitle:@"Stop!"];
-	[alertView setNumberOfRows:2];
+    if(IsMobileTimerAlarm(self))
+    {
+        UIAlertView *alertView = [self alertSheet];
+        
+        alertView.title = @"Alarm";
+        alertView.cancelButtonIndex = nil;
+        [alertView addButtonWithTitle:@"Snooze..."];
+        [alertView addButtonWithTitle:@"Stop!"];
+        [alertView setNumberOfRows:2];
+    }
+    else
+    {
+       %orig;
+    }
     
 }
 
